6f933e5e31d2f2052742bbc789fce5bcb81dd827
saracorreia07 <46967994+saracorreia07@users.noreply.github.com>
2021-03-25 09:36:12 +0000

    Initial commit

---
+++ Controllers/AppController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalSite.Controllers
+{
+    public class AppController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(object model)
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About me";
+
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    
+    var button = $("#orderButton");
+    button.on("click", function () {
+        alert("Buying Item");
+    });
+
+});
